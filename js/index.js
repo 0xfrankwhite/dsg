c09178dd5c06dd7dbf8746b355ccd12e0e1049eb
Benjamin E Deckey <benjaminedeckey@Benjamins-MacBook-Pro.local>
2021-11-19 12:47:52 -0700

    adding new server file

--- js/index.js
+++ js/index.js
@@ -1,9 +1,37 @@
 const http = require('http')
 const fs = require('fs')
 
-const server = https.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
+var htmlFile;
+var cssFile;
 
-server.listen(process.env.PORT || 3000)
+fs.readFile('index.html', function(err, data) {
+    if (err){
+        throw err;
+    }
+    htmlFile = data;
+});
+
+fs.readFile('./css/main.css', function(err, data) {
+    if (err){
+        throw err;
+    }
+    cssFile = data;
+});
+
+
+const server = http.createServer((req, res) => {
+  console.log(req.url)
+    switch (req.url) {
+        case "/css/main.css" :
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.write(cssFile);
+            break;
+        default :    
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(htmlFile);
+    };
+    res.end();
+  }
+)
+
+server.listen(process.env.PORT || 3000, () => {console.log(`Listening on port ${process.env.PORT || 3000}`)})
